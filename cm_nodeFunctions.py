d051b4ded42e1ffaa66dd99b35a5c7eac434c16a
John Roper <johnroper100@gmail.com>
2016-10-15 09:38:31 -0400

    start of overlapping actions

--- cm_nodeFunctions.py
+++ cm_nodeFunctions.py
@@ -655,11 +655,11 @@ class StateAction(State):
             tr = obj.animation_data.nla_tracks.new()  # NLA track
             action = actionobj.action  # bpy action
             if action:
-                currentFrame = bpy.context.scene.frame_current
+                currentFrame = bpy.context.scene.frame_current - 3
                 strip = tr.strips.new("", currentFrame, action)
                 strip.extrapolation = 'NOTHING'
                 strip.use_auto_blend = True
-            self.length = actionobj.length
+            self.length = actionobj.length + 3
 
             """tr = obj.animation_data.nla_tracks.new()  # NLA track
             action = actionobj.motion
@@ -671,6 +671,7 @@ class StateAction(State):
 
     def evaluateState(self):
         self.currentFrame += 1
+        self.currentFrame = self.currentFrame
 
         """Check to see if the current state is still playing an animation"""
         # print("currentFrame", self.currentFrame, "length", self.length)
